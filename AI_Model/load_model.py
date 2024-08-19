86212562a05173f968785a242fa3d774f9b0eba7
Manuel Manriquez <manriquezmanny@gmail.com>
2024-08-18 21:24:25 -0700

    Added timer and useful print statements

--- AI_Model/load_model.py
+++ AI_Model/load_model.py
@@ -16,10 +16,10 @@ model.to(device)
 model.eval()
 
 # Printing if GPU is available
-print("Using GPU") if torch.cuda.is_available() else print("No GPU detected.")
+print("Using GPU") if torch.cuda.is_available() else print("\nNo GPU detected. Defaulting to CPU\n")
 
 # Input text
-input_text = "Recommend a good medieval fantasy book, keep your response brief."
+input_text = "Briefly reccomend a fantasy book to read."
 
 # Tokenize input
 inputs = tokenizer(input_text, return_tensors="pt").to(device)
@@ -29,14 +29,14 @@ print("Generating a response...")
 start_time = time.time()
 # Generate response
 with torch.no_grad():
-    outputs = model.generate(**inputs, max_length=200)  # Reduced max_length for testing
+    outputs = model.generate(**inputs, max_length=100)  # Reduced max_length for testing
 
 # Ending timer and calculated elapsed time
 end_time = time.time()
 elapsed_time = end_time - start_time
 
-print("Decoding and printing response...")
+print("Decoding and printing response...\n")
 # Decode and print response
-response = tokenizer.decode(outputs[0], skip_special_tokens=True)
-print(f"Input: \n{input_text} \n Response: \n{response} \n\n Operation Time: {elapsed_time} seconds.")
+response = tokenizer.decode(outputs[0, len(inputs[0]):], skip_special_tokens=True)
+print(f"Input: {input_text}\n\nResponse: {response}\n\nOperation Time: {elapsed_time} seconds.")
 
