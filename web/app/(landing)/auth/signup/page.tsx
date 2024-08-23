867c3eb283bcb3647169fe00a74e95e7e7a4ab08
Alan Ibarra <alanjibarradev@gmail.com>
2024-08-22 21:02:46 -0700

    add login and signup page

--- web/app/(landing)/auth/signup/page.tsx
+++ web/app/(landing)/auth/signup/page.tsx
@@ -9,15 +9,13 @@ import {
 import { Input } from "@/components/ui/input";
 import Link from "next/link";
 
-export default function Login() {
+export default function Signup() {
   return (
     <div className="mx-auto flex h-screen max-w-96 items-center">
       <Card className="w-full">
         <CardHeader>
-          <h3 className="text-2xl font-semibold tracking-tight">Login</h3>
-          <p className="text-sm text-muted-foreground">
-            Welcome back! Please enter your details
-          </p>
+          <h3 className="text-2xl font-semibold tracking-tight">Sign Up</h3>
+          <p className="text-sm text-muted-foreground">Create your account</p>
         </CardHeader>
         <CardContent>
           <div className="space-y-6">
@@ -34,7 +32,7 @@ export default function Login() {
               <Input id="password" type="password" />
             </div>
             <Button className="w-full font-semibold text-gray-100">
-              Login
+              Sign up
             </Button>
           </div>
           <div className="my-5 flex items-center gap-3">
@@ -50,9 +48,9 @@ export default function Login() {
         <CardFooter className="flex justify-center p-4 pt-0">
           <Link
             className={`${buttonVariants({ variant: "link" })} text-gray-300`}
-            href="/auth/signup"
+            href="/auth/login"
           >
-            Don't have an account? Sign up for free
+            Already have an account? Login
           </Link>
         </CardFooter>
       </Card>
